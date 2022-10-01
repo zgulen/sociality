88cafcf1bdacfc0f3f393f610e682f31cad2fe20
zgulen <gulenzubeyir@gmail.com>
2022-10-01 16:50:11 +0300

    doing good

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { dateConverter } from "../utills/date";
 
 const Card = () => {
-    const [data1] = useState(data.posts_by_date["2021-06-17"]);
+    const [data0617] = useState(data.posts_by_date["2021-06-17"]);
     const socialIcon = (icon) => {
         if (icon === "facebook") {
             return <i class="fa-brands fa-facebook"></i>;
@@ -16,30 +16,70 @@ const Card = () => {
     };
     return (
         <div className="card-container">
-            {data1.map((e) => {
+            {data0617?.map((e) => {
                 console.log(e);
                 const {
                     published_at,
                     status,
                     account: { channel },
+                    is_published,
+                    entry: { message },
+                    entry: { image },
                 } = e;
                 return (
                     <div className="card">
-                        <div className={`status${status}`}>
-                            {socialIcon(channel)}
+                        <div className="card-type">
+                            <div className={`status status${status}`}>
+                                {socialIcon(channel)}
+                            </div>
                         </div>
-                        <div>
-                            <p>
-                            <time datetime={published_at}>
-                                {/* I didn't like this solutions but i couldn't find any  */}
-                                {`
-                                ${published_at.slice(8,10)} 
+                        <div className="time-icons">
+                            <p className="publish-time">
+                                <time datetime={published_at}>
+                                    {/* I didn't like this solutions but i couldn't find any  */}
+                                    {`
+                                ${published_at.slice(8, 10)} 
                                 ${dateConverter(published_at.slice(5, 7))} 
-                                ${published_at.slice(0,4)} - 
-                                ${published_at.slice(11,16)}
+                                ${published_at.slice(0, 4)} - 
+                                ${published_at.slice(11, 16)}
                                 `}
-                            </time>
+                                </time>
                             </p>
+                            <div className="card-icons">
+                                <span>
+                                    {is_published || (
+                                        <img
+                                            src="../icons/cancel.png"
+                                            alt="cancel icon"
+                                        ></img>
+                                    )}
+                                </span>
+                                <span>
+                                    <img
+                                        src="../icons/delete.png"
+                                        alt="delete icon"
+                                    />
+                                </span>
+                                <span>
+                                    <img
+                                        src="../icons/options.png"
+                                        alt="options icon"
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="card-entry">
+                            <p>{message.slice(0, message.indexOf('http'))} <a href="/">{message.slice(message.search('http'))}</a></p>
+                            <img
+                                width={288}
+                                src={image[0]}
+                                onError={({ currentTarget }) => {
+                                    console.log(currentTarget);
+                                    currentTarget.onerror = null; // prevents looping
+                                    currentTarget.src = "../nopostimg/no-post-image.png";
+                                }}
+                                alt="post"
+                            />
                         </div>
                     </div>
                 );
--- src/components/Facebook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Facebook = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default Facebook
